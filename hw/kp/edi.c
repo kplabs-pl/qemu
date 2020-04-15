4210be974b3f7f3807afbc8a3e4d32005765baab
Maciej Nowak <maciejt.nowak@gmail.com>
2020-04-15 19:06:29 +0200

    EDI registers

---
+++ hw/kp/edi.c
@@ -0,0 +1,130 @@
+#include "qemu/osdep.h"
+#include "qemu/module.h"
+#include "hw/qdev-core.h"
+#include "hw/qdev-properties.h"
+#include "qemu/log.h"
+#include "hw/core/cpu.h"
+#include "exec/memory.h"
+#include "exec/address-spaces.h"
+#include <nanomsg/pair.h>
+#include <nanomsg/reqrep.h>
+#include <nanomsg/pubsub.h>
+#include <nanomsg/pipeline.h>
+
+typedef struct KPEDIState {
+    DeviceState parent_obj;
+
+    uint64_t addr;
+
+    struct {
+        uint32_t command;
+        uint32_t pointer;
+        uint32_t size;
+        uint32_t interrupt;
+    } registers;
+} KPEDIState;
+
+#define TYPE_KP_EDI "kp-edi"
+#define KP_EDI(obj) OBJECT_CHECK(KPEDIState, (obj), \
+                                         TYPE_KP_EDI)
+
+static uint64_t kp_edi_register_read(void *opaque, hwaddr addr, unsigned size)
+{
+    KPEDIState *s = (KPEDIState*)opaque;
+
+    switch(addr)
+    {
+        case 0x00:
+            return s->registers.command;
+        case 0x04:
+            return s->registers.pointer;
+        case 0x08:
+            return s->registers.size;
+        case 0x0C:
+            return s->registers.interrupt;
+        default:
+            qemu_log("EDI: read invalid register %lX\n", addr);
+            return 0xDEADBEEF;
+    }
+}
+
+static void kp_edi_register_write(void *opaque, hwaddr addr, uint64_t data, unsigned size)
+{
+    KPEDIState *s = (KPEDIState*)opaque;
+
+    switch(addr)
+    {
+        case 0x00:
+            s->registers.command = (uint32_t)data;
+            break;
+        case 0x04:
+            s->registers.pointer = (uint32_t)data;
+            break;
+        case 0x08:
+            s->registers.size = (uint32_t)data;
+            break;
+        case 0x0C:
+            s->registers.interrupt = (uint32_t)data;
+            break;
+        default:
+            qemu_log("EDI: write invalid register %lX\n", addr);
+    }
+}
+
+static void kp_edi_reset(DeviceState* dev)
+{
+    // TODO: reset
+}
+
+static const MemoryRegionOps RegisterOps = {
+    .read = kp_edi_register_read,
+    .write = kp_edi_register_write,
+};
+
+static void kp_edi_realize(DeviceState* dev, Error** errp)
+{
+    KPEDIState *s = KP_EDI(dev);
+
+    MemoryRegion *system_memory = get_system_memory();
+
+    MemoryRegion *region = g_new(MemoryRegion, 1);
+
+    memory_region_init_io(region, NULL, &RegisterOps, s, "kp-edi", 4 * 4);
+
+    memory_region_add_subregion(system_memory, s->addr, region);
+
+    qemu_log("EDI realize\n");
+
+}
+
+static Property kp_edi_props[] = {
+    DEFINE_PROP_UINT64("addr", KPEDIState, addr, 0xF0000010),
+    DEFINE_PROP_END_OF_LIST(),
+};
+
+static void kp_edi_class_init(ObjectClass *klass, void *data)
+{
+    DeviceClass *dc = DEVICE_CLASS(klass);
+
+    dc->reset = kp_edi_reset;
+    dc->realize = kp_edi_realize;
+
+    device_class_set_props(dc, kp_edi_props);
+
+    dc->desc = "KP Labs - External Device Interface";
+    set_bit(DEVICE_CATEGORY_KP, dc->categories);
+}
+
+static TypeInfo kp_edi_info = {
+    .name = TYPE_KP_EDI,
+    .parent = TYPE_DEVICE,
+    .instance_size = sizeof(KPEDIState),
+    .class_init = kp_edi_class_init
+};
+
+static void kp_edi_register_type(void)
+{
+    type_register_static(&kp_edi_info);
+}
+
+type_init(kp_edi_register_type)
