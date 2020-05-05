da6227d1cdfca7ad1d86672bd575fca45f849710
Maciej Nowak <maciejt.nowak@gmail.com>
2020-05-05 12:48:58 +0200

    Linux build

---
+++ ci/configure-linux.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+BASE=$1
+INSTALL=$(realpath $2)
+
+export PKG_CONFIG_PATH=$INSTALL/lib/pkgconfig/
+# -static
+$BASE/configure \
+    --prefix=$INSTALL \
+    --bindir=$INSTALL/bin \
+    --target-list=arm-softmmu \
+    --disable-docs \
+    --disable-guest-agent \
+    --disable-sdl \
+    --disable-gtk \
+    --disable-vnc \
+    --disable-xen \
+    --disable-gnutls \
+    --disable-nettle \
+    --disable-auth-pam \
+    --disable-kvm \
+    --disable-cap-ng \
+    --disable-libusb \
+    --disable-vhost-net    \
+    --disable-vhost-vsock  \
+    --disable-vhost-scsi   \
+    --disable-vhost-crypto \
+    --disable-vhost-kernel \
+    --disable-vhost-user   \
+    --disable-live-block-migration \
+    --disable-replication \
+    --disable-parallels \
+    --disable-sheepdog \
+    --disable-crypto-afalg \
