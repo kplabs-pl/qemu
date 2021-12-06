be35f7b0ce7b10a21108b698fe2bdb6d45826d71
Przemysław Recha <precha@kplabs.pl>
2021-12-06 14:48:03 +0100

    Add properties to virt-microblaze machine

--- hw/microblaze/virt-microblaze.c
+++ hw/microblaze/virt-microblaze.c
@@ -6,6 +6,7 @@
 #include "exec/address-spaces.h"
 #include "hw/sysbus.h"
 #include "hw/qdev-properties.h"
+#include "qapi/visitor.h"
 
 #include "boot.h"
 
@@ -13,19 +14,17 @@ typedef struct {
     MachineState parent_obj;
 
     MicroBlazeCPU cpu;
+    uint64_t ram_base;
+    uint64_t intc_base;
+    uint64_t timer_base;
+    uint64_t uart_base;
+    uint32_t timer_irq;
 } VirtMicroblazeMachineState;
 
 #define TYPE_VIRT_MICROBLAZE_MACHINE MACHINE_TYPE_NAME("virt-microblaze")
 
 #define VIRT_MICROBLAZE_MACHINE(obj) \
     OBJECT_CHECK(VirtMicroblazeMachineState, obj, TYPE_VIRT_MICROBLAZE_MACHINE)
-
-#define RAM_BASEADDR 0x80000000
-#define UART_BASEADDR 0x40000000
-#define INTC_BASEADDR 0x40010000
-#define TIMER_BASEADDR 0x40020000
-
-#define TIMER_IRQ 0
 
 static void virt_microblaze_init(MachineState *ms) {
     VirtMicroblazeMachineState* m = VIRT_MICROBLAZE_MACHINE(ms);
@@ -34,23 +33,23 @@ static void virt_microblaze_init(MachineState *ms) {
     qemu_irq irq[32];
 
     object_initialize_child(OBJECT(m), "cpu", &m->cpu, TYPE_MICROBLAZE_CPU);
-    object_property_set_str(OBJECT(&m->cpu), "version", "8.40.b", &error_abort);
+    object_property_set_str(OBJECT(&m->cpu), "version", "9.6", &error_abort);
     object_property_set_uint(OBJECT(&m->cpu), "base-vectors",
-                             RAM_BASEADDR, &error_abort);
+                             m->ram_base, &error_abort);
     qdev_realize(DEVICE(&m->cpu), NULL, &error_abort);
     
     memory_region_init_ram(ram, NULL, "virt-microblaze.ram", ms->ram_size,
                            &error_fatal);
-    memory_region_add_subregion(system_memory, RAM_BASEADDR, ram);
+    memory_region_add_subregion(system_memory, m->ram_base, ram);
 
     DeviceState *dev;
     SysBusDevice *s;
 
     //create interrupt controller
     dev = qdev_new("xlnx.xps-intc");
-    qdev_prop_set_uint32(dev, "kind-of-intr", 1 << TIMER_IRQ);
+    qdev_prop_set_uint32(dev, "kind-of-intr", 1 << m->timer_irq);
     sysbus_realize_and_unref(SYS_BUS_DEVICE(dev), &error_fatal);
-    sysbus_mmio_map(SYS_BUS_DEVICE(dev), 0, INTC_BASEADDR);
+    sysbus_mmio_map(SYS_BUS_DEVICE(dev), 0, m->intc_base);
     sysbus_connect_irq(SYS_BUS_DEVICE(dev), 0,
                        qdev_get_gpio_in(DEVICE(&m->cpu), MB_CPU_IRQ));
     for (int i = 0; i < 32; i++) {
@@ -62,20 +61,80 @@ static void virt_microblaze_init(MachineState *ms) {
     s = SYS_BUS_DEVICE(dev);
     qdev_prop_set_chr(dev, "chardev", serial_hd(0));
     sysbus_realize_and_unref(s, &error_fatal);
-    sysbus_mmio_map(s, 0, UART_BASEADDR);
+    sysbus_mmio_map(s, 0, m->uart_base);
 
     // create timer
     dev = qdev_new("xlnx.xps-timer");
     qdev_prop_set_uint32(dev, "one-timer-only", 0);
     qdev_prop_set_uint32(dev, "clock-frequency", 100 * 1000000);
     sysbus_realize_and_unref(SYS_BUS_DEVICE(dev), &error_fatal);
-    sysbus_mmio_map(SYS_BUS_DEVICE(dev), 0, TIMER_BASEADDR);
-    sysbus_connect_irq(SYS_BUS_DEVICE(dev), 0, irq[TIMER_IRQ]);
+    sysbus_mmio_map(SYS_BUS_DEVICE(dev), 0, m->timer_base);
+    sysbus_connect_irq(SYS_BUS_DEVICE(dev), 0, irq[m->timer_irq]);
 
-    microblaze_load_kernel(&m->cpu, RAM_BASEADDR, ms->ram_size,
+    microblaze_load_kernel(&m->cpu, m->ram_base, ms->ram_size,
                            ms->initrd_filename,
                            ms->dtb,
                            NULL);
+}
+
+static void ram_base_set(Object *obj, Visitor *v, const char *name, void *opaque, Error **errp)
+{
+    VirtMicroblazeMachineState* m = VIRT_MICROBLAZE_MACHINE(obj);
+    visit_type_uint64(v, name, &m->ram_base, errp);
+}
+
+static void ram_base_get(Object *obj, Visitor *v, const char *name, void *opaque, Error **errp)
+{
+    VirtMicroblazeMachineState* m = VIRT_MICROBLAZE_MACHINE(obj);
+    visit_type_uint64(v, name, &m->ram_base, errp);
+}
+
+static void intc_base_set(Object *obj, Visitor *v, const char *name, void *opaque, Error **errp)
+{
+    VirtMicroblazeMachineState* m = VIRT_MICROBLAZE_MACHINE(obj);
+    visit_type_uint64(v, name, &m->intc_base, errp);
+}
+
+static void intc_base_get(Object *obj, Visitor *v, const char *name, void *opaque, Error **errp)
+{
+    VirtMicroblazeMachineState* m = VIRT_MICROBLAZE_MACHINE(obj);
+    visit_type_uint64(v, name, &m->intc_base, errp);
+}
+
+static void timer_base_set(Object *obj, Visitor *v, const char *name, void *opaque, Error **errp)
+{
+    VirtMicroblazeMachineState* m = VIRT_MICROBLAZE_MACHINE(obj);
+    visit_type_uint64(v, name, &m->timer_base, errp);
+}
+
+static void timer_base_get(Object *obj, Visitor *v, const char *name, void *opaque, Error **errp)
+{
+    VirtMicroblazeMachineState* m = VIRT_MICROBLAZE_MACHINE(obj);
+    visit_type_uint64(v, name, &m->timer_base, errp);
+}
+
+static void uart_base_set(Object *obj, Visitor *v, const char *name, void *opaque, Error **errp)
+{
+    VirtMicroblazeMachineState* m = VIRT_MICROBLAZE_MACHINE(obj);
+    visit_type_uint64(v, name, &m->uart_base, errp);
+}
+
+static void uart_base_get(Object *obj, Visitor *v, const char *name, void *opaque, Error **errp)
+{
+    VirtMicroblazeMachineState* m = VIRT_MICROBLAZE_MACHINE(obj);
+    visit_type_uint64(v, name, &m->uart_base, errp);
+}
+
+static void timer_irq_set(Object *obj, Visitor *v, const char *name, void *opaque, Error **errp)
+{
+    VirtMicroblazeMachineState* m = VIRT_MICROBLAZE_MACHINE(obj);
+    visit_type_uint32(v, name, &m->timer_irq, errp);
+}
+
+static void timer_irq_get(Object *obj, Visitor *v, const char *name, void *opaque, Error **errp)
+{
+    VirtMicroblazeMachineState* m = VIRT_MICROBLAZE_MACHINE(obj);
+    visit_type_uint32(v, name, &m->timer_irq, errp);
 }
 
 static void virt_microblaze_class_init(ObjectClass *oc, void *data)
@@ -83,10 +142,71 @@ static void virt_microblaze_class_init(ObjectClass *oc, void *data)
     MachineClass *mc = MACHINE_CLASS(oc);
     mc->desc = "Virtual Microblaze";
     mc->init = virt_microblaze_init;
+
+    object_class_property_add(
+        oc, "ram-base", "int",
+        &ram_base_get, &ram_base_set, NULL,
+        NULL
+    );
+
+    object_class_property_set_description(
+        oc, "ram-base",
+        "RAM base address"
+    );
+
+    object_class_property_add(
+        oc, "intc-base", "int",
+        &intc_base_get, &intc_base_set, NULL,
+        NULL
+    );
+
+    object_class_property_set_description(
+        oc, "intc-base",
+        "Interrupt controller base address"
+    );
+
+    object_class_property_add(
+        oc, "timer-base", "int",
+        &timer_base_get, &timer_base_set, NULL,
+        NULL
+    );
+
+    object_class_property_set_description(
+        oc, "timer-base",
+        "Timer base address"
+    );
+
+    object_class_property_add(
+        oc, "uart-base", "int",
+        &uart_base_get, &uart_base_set, NULL,
+        NULL
+    );
+
+    object_class_property_set_description(
+        oc, "uart-base",
+        "UART base address"
+    );
+    
+    object_class_property_add(
+        oc, "timer-irq", "int",
+        &timer_irq_get, &timer_irq_set, NULL,
+        NULL
+    );
+
+    object_class_property_set_description(
+        oc, "timer-irq",
+        "Timer IRQ number"
+    );
 }
 
 static void virt_microblaze_instance_init(Object* obj)
 {
+    VirtMicroblazeMachineState* m = VIRT_MICROBLAZE_MACHINE(obj);
+    m->ram_base = 0x00000000;
+    m->intc_base = 0x80000000;
+    m->timer_base = 0x80010000;
+    m->uart_base = 0x80020000;
+    m->timer_irq = 0;
 }
 
 static const TypeInfo virt_microblaze_type = {
